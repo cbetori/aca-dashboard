0880e1090816adb91618cb1cd450032e241bd4a3
Collin Betori <cbetori@virtusre.com>
2019-07-30 14:55:42 -0500

    Added production build

---
+++ client/build/precache-manifest.3e1afe7f3fe0f14df7d792af6e616412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d8b84ebfe62f47e112e5a6ea29a4283",
+    "url": "/index.html"
+  },
+  {
+    "revision": "097c77b7e9a0f923ad62",
+    "url": "/static/css/2.676dc204.chunk.css"
+  },
+  {
+    "revision": "ea4d347598084d364591",
+    "url": "/static/css/main.b2583ebc.chunk.css"
+  },
+  {
+    "revision": "097c77b7e9a0f923ad62",
+    "url": "/static/js/2.0c798d64.chunk.js"
+  },
+  {
+    "revision": "ea4d347598084d364591",
+    "url": "/static/js/main.a1ddb70e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
