cbaeb4043adef4c017b8e8f66c7f45eb1efbf670
Collin Betori <cbetori@virtusre.com>
2019-08-10 13:38:11 -0500

    switch from pool to client

--- server/queries.js
+++ server/queries.js
@@ -1,13 +1,15 @@
 require('dotenv').config('../.env')
-const  Pool  = require('pg').Pool
+const  Client  = require('pg').Pool
 
-const pool = new Pool({
+const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true
 })
 
+
+
 module.exports = {
     query: (text, params, callback) => {
-      return pool.query(text, params, callback)
+      return client.query(text, params, callback)
     },
   }
