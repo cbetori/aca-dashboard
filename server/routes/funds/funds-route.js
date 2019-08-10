44ed2ebbe90d10730a9255c8dd64baa3979e491d
Collin Betori <cbetori@virtusre.com>
2019-08-10 16:34:07 -0500

    updated server paths

--- server/routes/funds/funds-route.js
+++ server/routes/funds/funds-route.js
@@ -2,7 +2,7 @@ let express = require('express')
 let router = express.Router()
 const db = require('../../queries')
 
-router.get('/api/funds', (request, result, next) => {
+router.get('https://betori-db.herokuapp.com/api/funds', (request, result, next) => {
     db.query('SELECT * FROM "tblIDB_Funds"', null , (err, res) => {
       if (err) {
         return next(err)
